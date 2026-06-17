d7c0e7bb9eae1fcf14a37a242d99cb1948267ca7
Invitation <jumailahammed007@gmail.com>
2026-06-17 16:48:20 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Bendichal-Soccer-League-season-7/',
+       plugins: [react(), tailwindcss()],
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
